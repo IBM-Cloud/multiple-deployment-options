c67ba8dc7625cbf44e585f1d93eeeaee9e2935cc
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-04-24 15:06:18 +0200

    build latest

--- .bluemix/pipeline-BUILD-Docker.sh
+++ .bluemix/pipeline-BUILD-Docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd service
 
-export FULL_IMAGE_NAME=${REGISTRY_URL}/${IMAGE_NAME}
+export FULL_IMAGE_NAME=${REGISTRY_URL}/${IMAGE_NAME}:latest
 echo "Using Docker image ${FULL_IMAGE_NAME}"
 docker build -t ${FULL_IMAGE_NAME} .
 
