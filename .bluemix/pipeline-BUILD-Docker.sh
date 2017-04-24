ac826e4d58935f7e77d73fa9b4307111064b8498
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-04-24 15:41:42 +0200

    copy the Kubernetes deploy file

--- .bluemix/pipeline-BUILD-Docker.sh
+++ .bluemix/pipeline-BUILD-Docker.sh
@@ -5,7 +5,10 @@ export FULL_IMAGE_NAME=${REGISTRY_URL}/${IMAGE_NAME}:latest
 echo "Using Docker image ${FULL_IMAGE_NAME}"
 docker build -t ${FULL_IMAGE_NAME} .
 
-echo "Generating Kubernetes deployment file in artifact directory"
+echo "Preparing archive for Kubernetes deployment..."
+
+mkdir $ARCHIVE_DIR/.bluemix
+cp ../.bluemix/pipeline-DEPLOY-Kubernetes.sh $ARCHIVE_DIR/.bluemix/
 
 mkdir $ARCHIVE_DIR/service
 echo "IMAGE_NAME=${FULL_IMAGE_NAME}" > $ARCHIVE_DIR/service/image.env
